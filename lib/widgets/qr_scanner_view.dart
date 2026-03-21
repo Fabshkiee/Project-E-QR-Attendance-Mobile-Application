fd9e9d99eed93ff8efe75a13723decab8ccbc540
Fabshkiee <pwedefabros@gmail.com>
2026-03-22 05:41:36 +0800

    feat: QR Scanner Animation

--- lib/widgets/qr_scanner_view.dart
+++ lib/widgets/qr_scanner_view.dart
@@ -1,31 +1,86 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
-import '../../../core/theme/app_colors.dart';
+import '../core/theme/app_colors.dart';
 
-class QRScannerView extends StatelessWidget {
+class QRScannerView extends StatefulWidget {
   final Function(BarcodeCapture) onDetect;
   final double overlaySize;
 
   const QRScannerView({
     super.key,
     required this.onDetect,
-    this.overlaySize = 300,
+    this.overlaySize = 280,
   });
+
+  @override
+  State<QRScannerView> createState() => _QRScannerViewState();
+}
+
+class _QRScannerViewState extends State<QRScannerView>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize the animation to repeat back and forth
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    )..repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        // 1. Camera View
-        MobileScanner(onDetect: onDetect),
+        MobileScanner(onDetect: widget.onDetect),
 
-        // 2. The Overlay Frame
         Center(
           child: SizedBox(
-            width: overlaySize,
-            height: overlaySize,
+            width: widget.overlaySize,
+            height: widget.overlaySize,
             child: Stack(
               children: [
+                //Scanning Animation Line
+                AnimatedBuilder(
+                  animation: _animationController,
+                  builder: (context, child) {
+                    return Positioned(
+                      top: 50 + (_animationController.value * 185),
+                      left: 38,
+                      right: 38,
+                      child: child!,
+                    );
+                  },
+                  child: Container(
+                    height: 2,
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: AppColors.primaryAction.withValues(alpha: 1),
+                          blurRadius: 12,
+                          spreadRadius: 3,
+                        ),
+                      ],
+                      gradient: LinearGradient(
+                        colors: [
+                          AppColors.primaryAction.withValues(alpha: 1),
+                          AppColors.primaryAction,
+                          AppColors.primaryAction.withValues(alpha: 1),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+
+                // 2. Your Corners (on top of the line)
                 Align(
                   alignment: Alignment.topLeft,
                   child: _corner(alignment: Alignment.topLeft),
@@ -50,17 +105,16 @@ class QRScannerView extends StatelessWidget {
     );
   }
 
-  //helper to make a corner piece
   Widget _corner({required Alignment alignment}) {
-    const double side = 60; // size of the corner area
+    const double side = 60;
     const double thickness = 4;
     const double radius = 12;
 
     return ClipRect(
       child: Align(
         alignment: alignment,
-        widthFactor: 0.5, // only show half the width
-        heightFactor: 0.5, // only show half the height
+        widthFactor: 0.5,
+        heightFactor: 0.5,
         child: Container(
           width: side * 2,
           height: side * 2,
