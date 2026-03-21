438debdb3391fc956849f3546916905f049431e9
Fabshkiee <pwedefabros@gmail.com>
2026-03-22 04:04:31 +0800

    feat: add page app bar

--- lib/features/qr_scanner/qr_scanner_page.dart
+++ lib/features/qr_scanner/qr_scanner_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:project_e_qr_app/core/theme/app_colors.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class QRScannerPage extends StatelessWidget {
   const QRScannerPage({super.key});
@@ -6,12 +9,70 @@ class QRScannerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //Header
       appBar: AppBar(
-        title: const Text('Project-E Scanner'),
+        toolbarHeight: 90.0,
+        backgroundColor: AppColors.surfacePrimary,
+        foregroundColor: AppColors.textPrimary,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              'assets/images/proje_logo.svg',
+              width: 60,
+              height: 60,
+            ),
+            const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Project-E Fitness',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontFamily: 'Teko',
+                    fontWeight: FontWeight.w500,
+                    letterSpacing: 0.6,
+                  ),
+                ),
+                Text(
+                  'Reception Scanner',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontFamily: 'Lexend',
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
-      body: const Center(
-        child: Text('This is the QR Scanner Page'),
+      //Body
+      body: Stack(
+        children: [
+          MobileScanner(
+            onDetect: (capture) {
+              final List<Barcode> barcodes = capture.barcodes;
+              for (final barcode in barcodes) {
+                debugPrint('Found barcode: ${barcode.rawValue}');
+              }
+            },
+          ),
+          //Overlay
+          Center(
+            child: Container(
+              width: 300,
+              height: 300,
+              decoration: BoxDecoration(
+                border: Border.all(color: AppColors.primaryAction, width: 2),
+              ),
+            ),
+          ),
+        ],
       ),
+      //Register button
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.pushNamed(context, '/registration');
