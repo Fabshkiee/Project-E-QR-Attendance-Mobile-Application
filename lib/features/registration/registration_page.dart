dda27c35b7da911ea21a90fc2818c96270015208
Fabshkiee <pwedefabros@gmail.com>
2026-03-28 19:33:03 +0800

    feat: add registration appbar

--- lib/features/registration/registration_page.dart
+++ lib/features/registration/registration_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project_e_qr_app/core/theme/app_colors.dart';
 
 class RegistrationPage extends StatelessWidget {
   const RegistrationPage({super.key});
@@ -6,8 +7,34 @@ class RegistrationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.background,
+
       appBar: AppBar(
-        title: const Text('New Member Registration'),
+        toolbarHeight: 90,
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leadingWidth: 100,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            iconSize: 30,
+            color: Colors.white,
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        title: const Text(
+          'Member Registration',
+          style: TextStyle(
+            fontSize: 17,
+            fontFamily: 'Lexend',
+            fontWeight: FontWeight.w700,
+            color: AppColors.textPrimary,
+          ),
+        ),
       ),
       body: Center(
         child: Column(
