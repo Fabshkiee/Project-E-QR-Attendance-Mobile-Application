2cbdd0f0072d7bf625d0b0a1401bd23d96930419
Fabshkiee <pwedefabros@gmail.com>
2026-03-29 19:14:12 +0800

    refactor: text field to reusable widget with TextEditingController

---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import '../core/theme/app_colors.dart';
+
+class CustomAppTextField extends StatelessWidget {
+  final String hintText;
+  final IconData icon;
+  final TextEditingController? controller;
+  final String? Function(String?)? validator;
+  final TextInputAction? textInputAction;
+  final TextInputType? keyboardType;
+
+  const CustomAppTextField({
+    super.key,
+    required this.hintText,
+    required this.icon,
+    this.controller,
+    this.validator,
+    this.textInputAction,
+    this.keyboardType,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 62,
+      child: TextFormField(
+        controller: controller,
+        validator: validator,
+        textInputAction: textInputAction,
+        keyboardType: keyboardType,
+        style: const TextStyle(
+          fontSize: 14,
+          fontFamily: 'Lexend',
+          fontWeight: FontWeight.w400,
+          color: AppColors.textPrimary,
+        ),
+        decoration: InputDecoration(
+          hintText: hintText,
+          hintStyle: const TextStyle(
+            fontSize: 14,
+            fontFamily: 'Lexend',
+            fontWeight: FontWeight.w400,
+            color: AppColors.inputFieldText,
+          ),
+          prefixIcon: Icon(icon),
+          prefixIconColor: AppColors.textSubtle,
+          filled: true,
+          fillColor: AppColors.surfacePrimary,
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(18),
+            borderSide: BorderSide(color: Colors.white.withValues(alpha: 0.05)),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(18),
+            borderSide: const BorderSide(color: AppColors.textHighlight),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(18),
+            borderSide: const BorderSide(color: Colors.redAccent),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(18),
+            borderSide: const BorderSide(color: Colors.redAccent),
+          ),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(18)),
+        ),
+      ),
+    );
+  }
+}
