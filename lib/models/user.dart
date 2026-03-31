0af78179f8432437a3f855f8216cd86df1839470
Dyestrm <johnmark.panganiban@wvsu.edu.ph>
2026-03-31 11:26:58 +0800

    feat: Implement UserMapper in `user.dart`

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -13,3 +13,27 @@ class User {
 
 }
 
+// Maps user data from object to JSON and vice versa
+// NOTE: Fields must match database field names
+class UserMapper {	
+  // Maps JSON data to User object
+  User fromJson(Map<String, dynamic> json) {
+    return User(
+      fullName: json['full_name'],
+      nickname: json['nickname'],
+      membershipTier: json['membership_type_id'],
+      duration: json['valid_until'],
+    );
+  }
+
+  // Maps User object to JSON data
+  Map<String, dynamic> toJson(User user) {
+    return {
+      'full_name': user.fullName,
+      'nickname': user.nickname,
+      'membership_type_id': user.membershipTier,
+      'valid_until': user.duration,
+    };
+  }
+}
+
