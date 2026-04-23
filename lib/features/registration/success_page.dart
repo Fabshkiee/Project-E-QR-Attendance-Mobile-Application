c49915e5070797e1a9dda0e7d7ca060d802d5391
Fabshkiee <pwedefabros@gmail.com>
2026-04-23 13:45:46 +0800

    feat: add qr functionality to successful page

--- lib/features/registration/success_page.dart
+++ lib/features/registration/success_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:project_e_qr_app/core/theme/app_colors.dart';
 
 class SuccessPage extends StatelessWidget {
   const SuccessPage({super.key});
@@ -6,14 +9,67 @@ class SuccessPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.background,
       appBar: AppBar(
-        title: const Text('Registration Success'),
+        toolbarHeight: 90,
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leadingWidth: 100,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            iconSize: 30,
+            color: Colors.white,
+            onPressed: () => Navigator.pop(context),
+          ),
+        ),
+        title: const Text(
+          'Registration Successful!',
+          style: TextStyle(
+            fontSize: 17,
+            fontFamily: 'Lexend',
+            fontWeight: FontWeight.w700,
+            color: AppColors.textPrimary,
+          ),
+        ),
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            const Text('Member Successfully Registered!'),
+            SvgPicture.asset(
+              'assets/images/checkmark.svg',
+              width: 120,
+              height: 120,
+            ),
+            Text(
+              'Member Successfully\nRegistered!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 24,
+                fontFamily: 'Lexend',
+                fontWeight: FontWeight.w700,
+                color: AppColors.textPrimary,
+              ),
+            ),
+            Text('Member Name'),
+            SizedBox(height: 20),
+            Container(
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: QrImageView(
+                data: 'PROJE:MEM:0001:aa1324',
+                version: QrVersions.auto,
+                size: 200,
+                gapless: false,
+                backgroundColor: Colors.transparent,
+              ),
+            ),
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
