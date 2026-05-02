74af408d3ca6a6321b6f7cb18c1fca0023341d60
Dyestrm <johnmark.panganiban@wvsu.edu.ph>
2026-05-02 14:51:53 +0800

    feat: Add Member Registration Service Class

---
+++ lib/services/member_registration_service.dart
@@ -0,0 +1,7 @@
+import 'dart:math';
+
+import 'package:project_e_qr_app/main.dart';
+
+/// Handles credential generation, staff qr validation, 
+/// and write to local db operations for new users
+class MemberRegistrationService {}
