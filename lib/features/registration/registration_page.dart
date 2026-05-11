2dcc514fd72c5de11b888a9bc8f2ad96695285f9
Dyestrm <johnmark.panganiban@wvsu.edu.ph>
2026-05-11 11:45:30 +0800

    Revert "Merge branch 'feat/issue8' of https://github.com/Fabshkiee/Project-E-QR-Attendance-Mobile-Application into feat/issue8"
    This reverts commit 60eba6c89b099b892fcee0ec5efcdead3804d225, reversing
    changes made to ba2bb71988ea2046269dadd67532ac2c4a0e469f.

--- lib/features/registration/registration_page.dart
+++ lib/features/registration/registration_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:project_e_qr_app/core/theme/app_colors.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:project_e_qr_app/services/member_registration_service.dart';
 import 'package:project_e_qr_app/widgets/custom_text_field.dart';
 import 'package:project_e_qr_app/widgets/custom_dropdown.dart';
 import 'package:project_e_qr_app/widgets/form_label.dart';
